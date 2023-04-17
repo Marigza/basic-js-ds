f4e1ce07604122203946d019be32606501f21e65
Marigza <marigola@mail.ru>
2023-04-17 20:21:08 +0300

    feat: create stack function

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.stack = [];
+    this.length = 0;
+  }
+  push(element) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.stack.push(element);
+    this.length++;
+    return this.stack;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.length !== 0) {
+      this.length--;
+      return this.stack.pop();
+    }
+    
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+  return this.stack[this.length-1]
+
   }
 }
 
